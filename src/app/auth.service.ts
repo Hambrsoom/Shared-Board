626ad0dcfd611ea9f9f4475a028b2aee4c5dc2df
Cesare Spinoso <cesare.spinoso@gmail.com>
2018-12-24 10:36:26 -0500

    Added user login authentication

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,9 +6,14 @@ import {HttpClient} from '@angular/common/http'
 export class AuthService {
 
   private _registerUrl = "http://localhost:3001/api/register"
+  private _loginUrl = "http://localhost:3001/api/login"
   constructor(private http: HttpClient) { }
 
   registerUser(user){
     return this.http.post<any>(this._registerUrl, user)
+  }
+
+  loginUser(user){//user is an object that should be username OR email
+    return this.http.post<any>(this._loginUrl, user)
   }
 }
