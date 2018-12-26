28656c85bfdbb1d048212a84200ea5ef1d9fc8fa
Cesare Spinoso <cesare.spinoso@gmail.com>
2018-12-26 15:51:10 -0500

    Updated homepage animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupsService } from '../groups.service';
-
+import  Typed  from 'typed.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,6 +11,20 @@ export class HomeComponent implements OnInit {
   constructor(private _groupService: GroupsService) { }
 
   ngOnInit() {
+    /***For the title animation****/
+    const options = {
+      strings: ['Innovation', 'Discovery', 'ShareBoard'],
+      typeSpeed: 80,
+      backSpeed: 80,
+      showCursor: true,
+      cursorChar: '|',
+      loop: false
+    };
+
+    const typed = new Typed('.typed-element', options);
+     /***                      ***/
+
+
     this._groupService.getGroups().subscribe(
       res => this.groupss = res,
       err => console.log(err)
