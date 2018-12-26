a54fa5196e1093f6188d7c9f87c25f47af68e135
Hambrsoom <hampico1234@gmail.com>
2018-12-26 09:13:04 -0500

    IDK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
-<<<<<<< HEAD
 import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
   { path: '', redirectTo: "/login", pathMatch: "full" },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent}
-=======
-import {GroupsComponent} from "./groups/groups.component";
-import {RegisterComponent} from "./register/register.component";
-import {HomeComponent} from "./home/home.component";
 
-
-const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'register', component: RegisterComponent},
-  {path : 'login', component: LoginComponent }
->>>>>>> 690a5d44eba41791ade571844d868e4147b5c93e
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { GroupsService } from '../groups.service';
-=======
 
->>>>>>> 690a5d44eba41791ade571844d868e4147b5c93e
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-<<<<<<< HEAD
   groupss = []
   constructor(private _groupService: GroupsService) { }
 
   ngOnInit() {
     this._groupService.getGroups().subscribe(
-        res => this.groupss = res,
-        err => console.log(err)
+      res => this.groupss = res,
+      err => console.log(err)
     )
     console.log(this.groupss[0])
-=======
-
-  constructor() { }
-
-  ngOnInit() {
-
->>>>>>> 690a5d44eba41791ade571844d868e4147b5c93e
+  }
   }
 
-}
